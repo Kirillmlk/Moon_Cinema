3fe0c67aaeb685d1aa6895435498402897841048
Kirillmlk <kostakov.kiirill@yandex.by>
2024-09-14 21:23:45 +0300

    validation of data sent by the client

--- src/Controllers/MovieController.php
+++ src/Controllers/MovieController.php
@@ -3,6 +3,8 @@
 namespace App\Controllers;
 
 use App\Kernel\Controller\Controller;
+use App\Kernel\Validator\Validator;
+
 //use App\Kernel\View\View;
 
 class MovieController extends Controller
@@ -19,6 +21,13 @@ class MovieController extends Controller
 
     public function store(): void
     {
+        $data = ['name' => 'Kirill'];
+        $rules = ['name' => ['required', 'min:3', 'max:100']];
+
+        $validator = new Validator();
+
+        dd($validator->validate($data, $rules), $validator->errors());
+
         dd($this->request()->input('name'));
 
     }
