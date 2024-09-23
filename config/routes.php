b199335fe0697711e871474fa824ab3bc588da6a
Kirillmlk <kostakov.kiirill@yandex.by>
2024-09-23 21:36:56 +0300

    adding styles and new components

--- config/routes.php
+++ config/routes.php
@@ -9,13 +9,13 @@ use App\Middleware\AuthMiddleware;
 use App\Middleware\GuestMiddleware;
 
 return [
-    Route::get('/home', [HomeController::class, 'index']),
-    Route::get('/movies', [MovieController::class, 'index']),
-    Route::get('/admin/movies/add', [MovieController::class, 'add'], [AuthMiddleware::class]),
-    Route::post('/admin/movies/add', [MovieController::class, 'store']),
-    Route::get('/register', [RegisterController::class, 'index'], [GuestMiddleware::class]),
-    Route::post('/register', [RegisterController::class, 'register']),
-    Route::get('/login', [LoginController::class, 'index'], [GuestMiddleware::class]),
-    Route::post('/login', [LoginController::class, 'login']),
-    Route::post('/logout', [LoginController::class, 'logout']),
+    Route::get('/', [HomeController::class, 'index']),
 ];
+//    Route::get('/movies', [MovieController::class, 'index']),
+//    Route::get('/admin/movies/add', [MovieController::class, 'add'], [AuthMiddleware::class]),
+//    Route::post('/admin/movies/add', [MovieController::class, 'store']),
+//    Route::get('/register', [RegisterController::class, 'index'], [GuestMiddleware::class]),
+//    Route::post('/register', [RegisterController::class, 'register']),
+//    Route::get('/login', [LoginController::class, 'index'], [GuestMiddleware::class]),
+//    Route::post('/login', [LoginController::class, 'login']),
+//    Route::post('/logout', [LoginController::class, 'logout']),
--- views/components/start.php
+++ views/components/start.php
@@ -5,14 +5,16 @@
 ?>
 
 <!doctype html>
-<html lang="en">
+<html lang="ru" data-bs-theme="dark">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/assets/css/app.css">
     <title>Document</title>
+    <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/assets/css/app.css">
+    <script src="/assets/js/color-modes.js"></script>
 </head>
-<body>
-<?php $view->component('header') ?>
+<body class="d-flex flex-column min-vh-100">
+
+<?php $view->component('header'); ?>
